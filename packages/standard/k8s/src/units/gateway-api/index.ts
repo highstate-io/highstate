6cfb09aaf9a7108a04d512394a191555215c49f7
Fedor Chubukov <exeteres@proton.me>
2026-07-15 10:59:14 +0300

    feat(@highstate/k8s): configure gateway api versions

--- packages/standard/k8s/src/units/gateway-api/index.ts
+++ packages/standard/k8s/src/units/gateway-api/index.ts
@@ -3,14 +3,16 @@ import { forUnit } from "@highstate/pulumi"
 import { yaml } from "@pulumi/kubernetes"
 import { getProviderAsync } from "../../shared"
 
-const { inputs, outputs } = forUnit(k8s.gatewayApi)
+const { args, inputs, outputs } = forUnit(k8s.gatewayApi)
 
 const provider = await getProviderAsync(inputs.k8sCluster)
+const version = args.version ?? getDefaultVersion(args.channel)
+const manifestName = args.channel === "stable" ? "standard-install" : "experimental-install"
 
 new yaml.v2.ConfigFile(
   "gateway-api",
   {
-    file: "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml",
+    file: `https://github.com/kubernetes-sigs/gateway-api/releases/download/v${version}/${manifestName}.yaml`,
   },
   { provider },
 )
@@ -18,3 +20,11 @@ new yaml.v2.ConfigFile(
 export default outputs({
   k8sCluster: inputs.k8sCluster,
 })
+
+function getDefaultVersion(channel: "stable" | "experimental"): string {
+  if (channel === "experimental") {
+    throw new Error("Gateway API version must be specified when using the experimental channel.")
+  }
+
+  return "1.6.0"
+}
